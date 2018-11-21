ea2ce5b1383a727463cbf0cc1742fd3d9c1b9e61
liubaichuan <s1139@DESKTOP-8H5BLCC>
2018-11-21 11:16:08 +0800

    创建项目

---
+++ src/main/java/com/lbc/ticketplus/TicketplusApplication.java
@@ -0,0 +1,13 @@
+package com.lbc.ticketplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketplusApplication.class, args);
+        System.out.println("program running");
+    }
+}
