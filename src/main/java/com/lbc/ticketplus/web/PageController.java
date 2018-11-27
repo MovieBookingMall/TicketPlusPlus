91797923b6e22398bd9636e2a06f3587c796c3a9
liubaichuan <1139058537@qq.com>
2018-11-27 21:36:55 +0800

    增加影院信息页面、选择电影场次页面、选择座位页面

---
+++ src/main/java/com/lbc/ticketplus/web/PageController.java
@@ -0,0 +1,41 @@
+package com.lbc.ticketplus.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class PageController {
+
+    private final Logger log = LoggerFactory.getLogger(PageController.class);
+    @RequestMapping(value = "/hello")
+    @ResponseBody
+    public String helloWorld() {
+        log.info("/hello");
+        return "hello world";
+    }
+
+    @RequestMapping("/index")
+    public String  getHomePage(Model model) {
+        return "index";
+    }
+
+    @RequestMapping("/cinemaSelection")
+    public String  getCinemaSelectionPage(Model model) {
+        return "cinemaSelection";
+    }
+
+    @RequestMapping("/cinemaInfo")
+    public String  getCinemaInfoPage(Model model) {
+        return "cinemaInfo";
+    }
+
+    @RequestMapping("/selectSeat")
+    public String  getSelectSeatPage(Model model) {
+        return "selectSeat";
+    }
+}
