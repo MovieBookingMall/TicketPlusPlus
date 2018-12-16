517b2ba63dacdcfbac930acee79b369e4d4c51d6
liubaichuan <1139058537@qq.com>
2018-12-16 21:46:49 +0800

    增加计算用户相似度接口

--- src/main/java/com/lbc/ticketplus/tool/UserCF.java
+++ src/main/java/com/lbc/ticketplus/tool/UserCF.java
@@ -1,13 +1,10 @@
 package com.lbc.ticketplus.tool;
 
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
+import org.junit.Test;
+
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Scanner;
-import java.util.Set;
 
 /**
  * 基于用户的协同过滤推荐算法实现
@@ -17,6 +14,83 @@ import java.util.Set;
  * D c d e
  */
 public class UserCF {
+
+    @Test
+    public void testGetCosineSimilarity() {
+        List<String> _userItems = new ArrayList<String>();
+        _userItems.add("0 a b c d e g");
+        _userItems.add("1 a b c e f h");
+        _userItems.add("2 b c d e f g");
+        _userItems.add("3 a b c");
+        Map<String, Double> cosineSimilarity = getCosineSimilarity(1, _userItems);
+        Set<Entry<String, Double>> entries = cosineSimilarity.entrySet();
+        for (Entry e : entries) {
+            System.out.println("similarity with " + e.getKey() + ": " + e.getValue());
+        }
+
+    }
+
+    /**
+     * 基于用户的协同过滤推荐算法计算余弦相似度
+     *
+     * @param _userItems * 用户ID	物品ID集合
+     *                   A		a b d
+     *                   B		a c
+     *                   C		b e
+     *                   D		c d e
+     * @return
+     */
+    public static Map<String, Double> getCosineSimilarity(Integer _userId, List<String> _userItems) {
+        int N = _userItems.size();
+        int[][] sparseMatrix = new int[N][N];    //建立用户稀疏矩阵，用于用户相似度计算【相似度矩阵】
+        Map<String, Integer> userItemLength = new HashMap<>();    //存储每一个用户对应的不同物品总数  eg: A 3
+        Map<String, Set<String>> itemUserCollection = new HashMap<>();    //建立物品到用户的倒排表 eg: a A B
+        Set<String> items = new HashSet<>();    //辅助存储物品集合
+        Map<String, Integer> userID = new HashMap<>();    //辅助存储每一个用户的用户ID映射
+        Map<Integer, String> idUser = new HashMap<>();    //辅助存储每一个ID对应的用户映射
+        for (int i = 0; i < N; i++) {
+            String[] user_item = _userItems.get(i).split(" ");
+            int length = user_item.length;
+            userItemLength.put(user_item[0], length - 1);    //eg: A 3
+            userID.put(user_item[0], i);    //用户ID与稀疏矩阵建立对应关系
+            idUser.put(i, user_item[0]);
+            //建立物品--用户倒排表
+            for (int j = 1; j < length; j++) {
+                if (items.contains(user_item[j])) {    //如果已经包含对应的物品--用户映射，直接添加对应的用户
+                    itemUserCollection.get(user_item[j]).add(user_item[0]);
+                } else {    //否则创建对应物品--用户集合映射
+                    items.add(user_item[j]);
+                    itemUserCollection.put(user_item[j], new HashSet<String>());    //创建物品--用户倒排关系
+                    itemUserCollection.get(user_item[j]).add(user_item[0]);
+                }
+            }
+        }
+        //计算相似度矩阵【稀疏】
+        Set<Entry<String, Set<String>>> entrySet = itemUserCollection.entrySet();
+        Iterator<Entry<String, Set<String>>> iterator = entrySet.iterator();
+        while (iterator.hasNext()) {
+            Set<String> commonUsers = iterator.next().getValue();
+            for (String user_u : commonUsers) {
+                for (String user_v : commonUsers) {
+                    if (user_u.equals(user_v)) {
+                        continue;
+                    }
+                    sparseMatrix[userID.get(user_u)][userID.get(user_v)] += 1;    //计算用户u与用户v都有正反馈的物品总数
+                }
+            }
+        }
+        //计算用户之间的相似度【余弦相似性】返回结果
+        Map<String, Double> result = new HashMap<>();
+        int recommendUserId = userID.get(_userId.toString());
+        for (int j = 0; j < sparseMatrix.length; j++) {
+            if (j != recommendUserId) {
+//                System.out.println(idUser.get(recommendUserId) + "--" + idUser.get(j) + "相似度:" + sparseMatrix[recommendUserId][j] / Math.sqrt(userItemLength.get(idUser.get(recommendUserId)) * userItemLength.get(idUser.get(j))));
+                double similarity = sparseMatrix[recommendUserId][j] / Math.sqrt(userItemLength.get(idUser.get(recommendUserId)) * userItemLength.get(idUser.get(j)));
+                result.put(idUser.get(j), similarity);
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         /**
